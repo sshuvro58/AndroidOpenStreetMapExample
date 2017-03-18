ddeff1c347effe9b85e3c1b2523cb44e175d90df
sshuvro58 <sshuvro58@gmail.com>
2017-03-19 02:03:50 +0600

    OSM added

--- app/src/main/java/me/shuvro/androidopenstreetmapexample/MainActivity.java
+++ app/src/main/java/me/shuvro/androidopenstreetmapexample/MainActivity.java
@@ -1,27 +1,40 @@
 package me.shuvro.androidopenstreetmapexample;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.kml.KmlDocument;
+import org.osmdroid.bonuspack.kml.KmlPoint;
+import org.osmdroid.bonuspack.location.OverpassAPIProvider;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.FolderOverlay;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MapEventsReceiver, MapView.OnFirstLayoutListener {
 
+    MapView map;
+    OverpassAPIProvider overpassProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Context ctx = getApplicationContext();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         //important! set your user agent to prevent getting banned from the osm servers
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         setContentView(R.layout.activity_main);
 
-        MapView map = (MapView) findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
         map.setUseDataConnection(true);
         map.setBuiltInZoomControls(true);
@@ -30,6 +43,27 @@ public class MainActivity extends AppCompatActivity {
         mapController.setZoom(14);
         GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
         mapController.setCenter(startPoint);
+
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+            }
+        }.execute();
+
+        overpassProvider = new OverpassAPIProvider();
+        String url = overpassProvider.urlForTagSearchKml("amenity=cinema", map.getBoundingBox(), 500, 30);
+        KmlDocument mkmlDocument = new KmlDocument();
+        boolean ok = overpassProvider.addInKmlFolder(mkmlDocument.mKmlRoot, url);
+        FolderOverlay kmlOverlay = (FolderOverlay) mkmlDocument.mKmlRoot.buildOverlay(map, null, null, mkmlDocument);
+        map.getOverlays().add(kmlOverlay);
+        map.getOverlays().add(kmlOverlay);
     }
 
     @Override
@@ -40,5 +74,20 @@ public class MainActivity extends AppCompatActivity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+    }
+
+    @Override
+    public boolean singleTapConfirmedHelper(GeoPoint p) {
+        return false;
+    }
+
+    @Override
+    public boolean longPressHelper(GeoPoint p) {
+        return false;
+    }
+
+    @Override
+    public void onFirstLayout(View v, int left, int top, int right, int bottom) {
+
     }
 }
